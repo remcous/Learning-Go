3c1b95a8fb9b47aa2e4f97b325618d2885bf4676
Remi Coussement <remcous@gmail.com>
2024-01-27 09:44:17 -0800

    Ch11 complete

---
+++ Ch11/serveMux.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func main() {
+	person := http.NewServeMux()
+	person.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("greetings!\n"))
+	})
+	dog := http.NewServeMux()
+	dog.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("good puppy!\n"))
+	})
+
+	mux := http.NewServeMux()
+	mux.Handle("/person/", http.StripPrefix("/person", person))
+	mux.Handle("/dog/", http.StripPrefix("/dog", dog))
+
+	s := http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 90 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      mux,
+	}
+
+	err := s.ListenAndServe()
+	if err != nil {
+		if err != http.ErrServerClosed {
+			panic(err)
+		}
+	}
+}
